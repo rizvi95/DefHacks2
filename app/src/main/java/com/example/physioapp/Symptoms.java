8075efc8248835c9bd2a7ad0f11299f5286b96d4
rizvi95 <33403076+rizvi95@users.noreply.github.com>
2020-06-21 10:12:00 -0600

    link symptoms page to main

--- app/src/main/java/com/example/physioapp/Symptoms.java
+++ app/src/main/java/com/example/physioapp/Symptoms.java
@@ -1,4 +1,34 @@
 package com.example.physioapp;
 
-public class Symptoms {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Symptoms extends AppCompatActivity {
+
+    private Button button_symptom;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.symptoms);
+
+
+        button_symptom = (Button) findViewById(R.id.button_symptoms);
+        button_symptom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSymptoms();
+            }
+        });
+    }
+
+    public void openSymptoms() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
